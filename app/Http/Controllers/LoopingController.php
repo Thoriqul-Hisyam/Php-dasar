6b54296ffb03dcdac7795a31d649834828b51079
Thoriqul-Hisyam <thoriqulh12@gmail.com>
2022-08-29 16:51:37 +0700

    tugas php dasar

---
+++ app/Http/Controllers/LoopingController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class LoopingController extends Controller
+{
+    public function loop(){
+        
+        for($i=1;$i<=100;$i++){
+            if($i%3 == 0 && $i%5 == 0){
+              echo number_format($luasp = $this -> luaspersegi($i/3,$i/5),2,".","")."<br>";
+              
+            }
+            else if($i%5 == 0){
+               
+                echo number_format( $keliling = $this ->kelilinglingakaran(3.14,$i/5),2,".","")."<br>";
+            }
+            else if($i%3 == 0){
+                echo $luas = $this->luaslingkaran(3.14,$i/3).'<br>';
+            }else{
+            echo number_format($i,2,".","")."<br>";
+             }
+        }
+
+    }
+
+    public function luaslingkaran($phi,$jarijari){
+        $luaslingkaran = $phi*$jarijari*$jarijari;
+        
+        return $luaslingkaran;
+    }
+
+    public function kelilinglingakaran($phi,$jarijari){
+      
+
+        $kelilinglingkaran = 2*$phi*$jarijari;
+
+        return $kelilinglingkaran;
+    }
+
+    public function luaspersegi($panjang,$lebar){
+       
+        $luaspersegi = $panjang*$lebar;
+        
+        return $luaspersegi;
+    }
+}
